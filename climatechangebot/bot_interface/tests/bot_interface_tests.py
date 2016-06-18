2644da0c84cef3194d981ab775c92d06ac51251c
vagrant <vagrant@vagrant.vm>
2016-06-18 19:11:22 +0000

    Finishing up the facebook bot interface

--- climatechangebot/bot_interface/tests/bot_interface_tests.py
+++ climatechangebot/bot_interface/tests/bot_interface_tests.py
@@ -10,7 +10,7 @@ import unittest
 import ConfigParser
 
 from config import Config
-from bot_interface.bot_interface import NotificationType, RecipientMethod, BotInterface
+from bot_interface.bot_interface import *
 
 config = ConfigParser.ConfigParser()
 config.read("local_test_config.cfg")
@@ -22,14 +22,6 @@ FB_MESSAGING_URL = (
 
 
 class TestBotInterface(unittest.TestCase):
-    def testNotificationTypeEnum(self):
-        self.assertEqual(NotificationType.REGULAR.value, "REGULAR")
-        self.assertEqual(NotificationType.SILENT_PUSH.value, "SILENT_PUSH")
-        self.assertEqual(NotificationType.NO_PUSH.value, "NO_PUSH")
-
-    def testRecipientMethodEnum(self):
-        self.assertEqual(RecipientMethod.ID.value, "id")
-        self.assertEqual(RecipientMethod.PHONE_NUMBER.value, "phone_number")
 
     def testBotInterfaceInit(self):
         bot = BotInterface(FB_MESSAGING_URL)
@@ -40,5 +32,59 @@ class TestBotInterface(unittest.TestCase):
         response = bot.send_text_message(config.get('SECRET', 'fb_test_recipient_id'),
             'Testing the bot interface testSendTextMessage', RecipientMethod.ID.value,
             NotificationType.REGULAR.value)
+        print(response)
 
         self.assertEqual(response.status_code, 200)
+
+    def testSendPayloadMessageImage(self):
+        bot = BotInterface(FB_MESSAGING_URL)
+        response = bot.send_image_payload_message(config.get('SECRET', 'fb_test_recipient_id'),
+            RecipientMethod.ID.value, NotificationType.REGULAR.value,
+            image_url="https://media-cdn.tripadvisor.com/media/photo-s/04/17/78/62/peruvian-andes-adventures.jpg")
+        print(response)
+        self.assertEqual(response.status_code, 200)
+
+    def testSendPayloadMessageGeneric(self):
+        bot = BotInterface(FB_MESSAGING_URL)
+
+        elements = []
+        
+        button = bot.create_button(button_type=ButtonType.WEBURL.value,
+            title="Test Button", url="http://www.nytimes.com/")
+        elements.append(bot.create_generic_template_element(element_title="Test Title 1", element_item_url="http://www.nytimes.com/",
+            element_image_url="https://media-cdn.tripadvisor.com/media/photo-s/04/17/78/62/peruvian-andes-adventures.jpg",
+            element_subtitle="the andes are so beautiful!!", element_buttons=[button]))
+
+        elements.append(bot.create_generic_template_element(element_title="Test Title 2", element_item_url="http://www.google.com/",
+            element_image_url="https://media-cdn.tripadvisor.com/media/photo-s/04/17/78/62/peruvian-andes-adventures.jpg",
+            element_subtitle="I would really like to go there."))
+
+        response = bot.send_generic_payload_message(config.get('SECRET', 'fb_test_recipient_id'),
+            RecipientMethod.ID.value, NotificationType.REGULAR.value,
+            elements=elements)
+        
+        self.assertEqual(response.status_code, 200)
+
+    def testSendPayloadMessageButton(self):
+        bot = BotInterface(FB_MESSAGING_URL)
+
+        # URL button
+        buttons = []
+        buttons.append(bot.create_button(button_type=ButtonType.WEBURL.value,
+            title="Test Button", url="http://www.nytimes.com/"))
+        buttons.append(bot.create_button(button_type=ButtonType.WEBURL.value,
+            title="Test Button 2", url="http://www.nytimes.com/"))
+        response = bot.send_button_payload_message(config.get('SECRET', 'fb_test_recipient_id'),
+            RecipientMethod.ID.value, NotificationType.REGULAR.value,
+            button_title="Test Button Title", buttons=buttons)
+        self.assertEqual(response.status_code, 200)
+
+        # Callback button
+        buttons = []
+        buttons.append(bot.create_button(button_type=ButtonType.POSTBACK.value,
+            title="Test Button", payload='USER_DEFINED_PAYLOAD_FOR_THIS_BUTTON'))
+        response = bot.send_button_payload_message(config.get('SECRET', 'fb_test_recipient_id'),
+            RecipientMethod.ID.value, NotificationType.REGULAR.value,
+            button_title="Test Button Title", buttons=buttons)
+        self.assertEqual(response.status_code, 200)
+
