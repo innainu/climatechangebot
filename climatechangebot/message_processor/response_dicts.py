4ae289f759c9180bfdca610ca80b89276dd9ae67
btabanpour <btabanpour@gmail.com>
2016-07-10 13:31:39 -0400

    fixing some bad gif links

--- climatechangebot/message_processor/response_dicts.py
+++ climatechangebot/message_processor/response_dicts.py
@@ -2,8 +2,8 @@
 
 gif_dict = {
     'TEMPERATURE': [
-        'https://scontent.xx.fbcdn.net/v/t34.0-12/13576458_10154119263035465_344080870_n.gif?_nc_ad=z-m&oh=d2fc20c68953b8b18b3f2680c95c55b8&oe=577B0E90',
-        'https://scontent.xx.fbcdn.net/v/t34.0-12/13578621_10154119263270465_589263997_n.gif?_nc_ad=z-m&oh=8f86b656f68845f16f4743e4ad55f56a&oe=577B0B73'
+        'https://media.giphy.com/media/hPovBcQ3c1g9W/giphy.gif',
+        'https://media.giphy.com/media/9u7xpfvuzD89W/giphy.gif'
     ],
 }
 
