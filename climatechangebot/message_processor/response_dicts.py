9a0266a754efc504b31d26574227621e5cf427b9
btabanpour <btabanpour@gmail.com>
2016-07-16 15:15:39 -0400

    Added gif and cursewords to being.rive

--- climatechangebot/message_processor/response_dicts.py
+++ climatechangebot/message_processor/response_dicts.py
@@ -3,7 +3,8 @@
 gif_dict = {
     'TEMPERATURE': [
         'https://media.giphy.com/media/hPovBcQ3c1g9W/giphy.gif',
-        'https://media.giphy.com/media/9u7xpfvuzD89W/giphy.gif'
+        'https://media.giphy.com/media/9u7xpfvuzD89W/giphy.gif',
+        'https://media.giphy.com/media/xTiTnpsAMlk0pJMFd6/giphy.gif'
     ],
 }
 
